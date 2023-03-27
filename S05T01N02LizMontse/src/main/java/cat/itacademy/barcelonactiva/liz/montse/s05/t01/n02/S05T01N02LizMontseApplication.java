0ff0a8883031072d3794c391a4a9caa4f7e56e59
montseliz <montse.liz92@gmail.com>
2023-03-27 10:29:10 +0200

    canvi nom api swagger

--- S05T01N02LizMontse/src/main/java/cat/itacademy/barcelonactiva/liz/montse/s05/t01/n02/S05T01N02LizMontseApplication.java
+++ S05T01N02LizMontse/src/main/java/cat/itacademy/barcelonactiva/liz/montse/s05/t01/n02/S05T01N02LizMontseApplication.java
@@ -1,11 +1,14 @@
 package cat.itacademy.barcelonactiva.liz.montse.s05.t01.n02;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Flowers Management System API", version = "1.0.0"))
 public class S05T01N02LizMontseApplication {
 
 	@Bean
